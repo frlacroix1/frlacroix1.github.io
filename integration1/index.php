a8a7ddd139ba774500ff544f15c7c5ee7e4818eb
frlacroix1 <frlacroix@csfoy.ca>
2025-08-12 14:18:57 -0400

    Ajout des redirections

---
+++ integration1/index.php
@@ -0,0 +1,8 @@
+<?php 
+  global $prms; 
+  $prms = array();
+  $prms['titre_lecon'] = "Intégration 1";
+?>
+<?php require_once('header.php'); ?>
+               
+<?php require_once('footer.php'); ?>
