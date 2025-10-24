fea2b67d865b30deb562875c082fcab39dc7c37e
frlacroix1 <frlacroix@csfoy.ca>
2025-10-24 12:48:38 -0400

    Lecon 9 - v20

--- integration1/js/functions.js
+++ integration1/js/functions.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function() {
-        var lecon_en_cours = 8;
+        var lecon_en_cours = 9;
         jQuery("header ul.lecons li").each(function(index, element) {
             index ++;
             console.log("List Item " + index + ": " + $(this).text());
