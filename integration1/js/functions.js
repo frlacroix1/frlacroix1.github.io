c601ddc6f5d69931bc3bc3f0e368c4e3745d6f3c
frlacroix1 <frlacroix@csfoy.ca>
2025-11-06 14:04:44 -0500

    Lecon 11 - v1

--- integration1/js/functions.js
+++ integration1/js/functions.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function() {
-        var lecon_en_cours = 10;
+        var lecon_en_cours = 11;
         jQuery("header ul.lecons li").each(function(index, element) {
             index ++;
             console.log("List Item " + index + ": " + $(this).text());
