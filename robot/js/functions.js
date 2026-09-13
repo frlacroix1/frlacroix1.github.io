13e8777000b98592bc5b2488f31a20fc6678338a
frlacroix1 <frlacroix@csfoy.ca>
2026-09-13 12:10:45 -0400

    Robot - style des listes ol

--- robot/js/functions.js
+++ robot/js/functions.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
     
-    jQuery('.theme').matchHeight();
+    jQuery('.theme span').matchHeight();
     jQuery('.equal').matchHeight();
 
     if ( jQuery("#mise-en-place").length ) {
